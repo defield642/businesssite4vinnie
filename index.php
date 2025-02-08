2a8c371c5a39754bd462c90d25186edff97b2494
defield642 <145449466+defield642@users.noreply.github.com>
2025-02-08 17:29:07 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<title>vincha</title>
+<body>
+    <h1>welcome place your order below</h1>
+
+</body>
+</html>
